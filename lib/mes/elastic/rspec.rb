f6bcb104a6f10dc3e0b38cb0bc699bb623e83432
Roman Lupiychuk <luckyjazzbo@gmail.com>
2016-07-12 13:25:37 +0200

    Load factories in rspec helper

--- lib/mes/elastic/rspec.rb
+++ lib/mes/elastic/rspec.rb
@@ -1,3 +1,6 @@
 require File.join(Mes::Elastic::ROOT, 'spec/support/elastic_index_helpers')
 require File.join(Mes::Elastic::ROOT, 'spec/support/with_eva_indices')
 require File.join(Mes::Elastic::ROOT, 'spec/support/with_mes_indices')
+
+require 'factory_girl'
+Dir[File.join(Mes::Elastic::ROOT, 'spec/factories/**/*.rb')].each { |file| require(file) }
