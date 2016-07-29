7f27ef5eaed98b9a749dafc75c573d388c2626cf
Anton Priadko <antonpriadko@gmail.com>
2016-07-29 16:13:52 +0300

    MES-791: Added mapping for licence_profiles profiles

--- app/models/mes/video.rb
+++ app/models/mes/video.rb
@@ -2,6 +2,7 @@ module Mes
   module Elastic
     class Video < Resource
       LANGS = %i(default en de).freeze
+      DEVICE_CLASSES=%i(BROWSER MOBILE SETTOPBOX SMARTTV HBBTV GAMECONSOLE HDMISTICK).freeze
 
       object :titles do
         LANGS.each do |lang|
@@ -21,6 +22,34 @@ module Mes
 
       object :image do
         field :url, type: :string, index: :not_analyzed
+      end
+
+      object :licence_profiles do
+        field :created_at,          type: :float
+        field :modified_at,         type: :float
+        field :version,             type: :float
+        field :status,              type: :string, index: :not_analyzed # %i(NOT_READY READY)
+        field :type,                type: :string, index: :not_analyzed # %i(SYNDICATION MDS_PROFILE)
+        field :id,                  type: :string, index: :not_analyzed
+        field :tenant_id,           type: :string, index: :not_analyzed
+        field :name,                type: :string
+        field :name_short,          type: :string
+        field :start_date,          type: :float
+        field :end_date,            type: :float
+        array :products,            type: :string, index: :not_analyzed # %i(FIXED_PRICE ADS)
+        array :sales_houses,        type: :string, index: :not_analyzed
+        array :geo_locations,       type: :string, index: :not_analyzed
+        array :device_classes,      type: :string, index: :not_analyzed # DEVICE_CLASSES
+        object :bandwidth_max do
+          DEVICE_CLASSES.each do |device_class|
+            field device_class, type: :integer
+          end
+        end
+        field :max_resolution,      type: :string, index: :not_analyzed # %i(SD HD720 HD1080 4K)
+        array :publisher_whitelist, type: :string, index: :not_analyzed
+        array :publisher_blacklist, type: :string, index: :not_analyzed
+        field :drm,                 type: :boolean
+        field :entitlement,         type: :boolean
       end
 
       object :ad_groups do
